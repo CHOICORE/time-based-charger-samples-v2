0a308281dc0769e5c2c162e1657601acbe0076de
Jae-hyeong Choi <choicore@parkinghub.co.kr>
2024-11-11 17:25:29 +0900

    feat: add domain class

---
+++ src/main/kotlin/me/choicore/samples/charge/domain/Charger.kt
@@ -0,0 +1,48 @@
+package me.choicore.samples.charge.domain
+
+import me.choicore.samples.charge.domain.ChargingStatus.CHARGED
+import me.choicore.samples.charge.domain.ChargingStatus.CHARGING
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class Charger(
+    private val chargingStationSelectorProvider: ChargingStationSelectorProvider,
+    private val specifiedDateChargingStrategyRepository: SpecifiedDateChargingStrategyRepository,
+    private val chargingTargetRepository: ChargingTargetRepository,
+) {
+    fun charge(
+        complexId: Long,
+        chargedOn: LocalDate,
+    ) {
+        val chargingTargets: List<ChargingTarget> =
+            chargingTargetRepository.findByComplexIdAndLastChargedOnLessThanEqualOrNull(
+                complexId = complexId,
+                lastChargedOn = chargedOn,
+            )
+
+        val chargingStationSelector: ChargingStationSelector =
+            chargingStationSelectorProvider.getChargingStationSelector(complexId = complexId)
+        val specifies: List<SpecifiedDateChargingStrategy> =
+            specifiedDateChargingStrategyRepository.findAllByComplexId(complexId = complexId)
+        val specifiedDateChargingStrategies = SpecifiedDateChargingStrategies(specifies)
+
+        for (chargingTarget: ChargingTarget in chargingTargets) {
+            var currentChargedOn: LocalDate = chargingTarget.lastChargedOn ?: chargingTarget.arrivedOn
+            while (currentChargedOn <= chargedOn) {
+                val chargingUnit: ChargingUnit = chargingTarget.getChargingUnit(chargedOn = currentChargedOn)
+                val chargingStrategies = specifiedDateChargingStrategies.getChargingStrategies(date = currentChargedOn)
+
+                if (chargingStrategies.isEmpty()) {
+                    val chargingStation: ChargingStation = chargingStationSelector.select(chargedOn = chargedOn)
+                    val dayOfWeekChargingStrategies: DayOfWeekChargingStrategies =
+                        chargingStation.dayOfWeekChargingStrategies
+                }
+
+                chargingTarget.lastChargedOn = currentChargedOn
+                chargingTarget.status = if (chargingTarget.departedOn == currentChargedOn) CHARGED else CHARGING
+                currentChargedOn = currentChargedOn.plusDays(1)
+            }
+        }
+    }
+}
