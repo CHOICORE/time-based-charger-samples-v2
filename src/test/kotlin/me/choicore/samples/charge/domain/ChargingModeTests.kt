c3b1132d636917613aae31ce57048a2fc85ab05d
Jae-hyeong Choi <choicore@parkinghub.co.kr>
2024-11-12 17:51:02 +0900

    test: 계산 테스트

--- src/test/kotlin/me/choicore/samples/charge/domain/ChargingModeTests.kt
+++ src/test/kotlin/me/choicore/samples/charge/domain/ChargingModeTests.kt
@@ -1,21 +1,31 @@
 package me.choicore.samples.charge.domain
 
 import me.choicore.samples.charge.domain.ChargingMode.Discharge
+import me.choicore.samples.charge.domain.ChargingMode.Standard
 import me.choicore.samples.charge.domain.ChargingMode.Surcharge
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class ChargingModeTests {
     @Test
     fun t1() {
         val surcharge = Surcharge(percentage = 10)
-        val charge = surcharge.charge(1000L)
-        println(charge.toLong())
+        val charge: BigDecimal = surcharge.charge(1000L)
+        assertThat(charge).isEqualByComparingTo(BigDecimal.valueOf(1100))
     }
 
     @Test
     fun t2() {
         val discharge = Discharge(percentage = 10)
-        val charge = discharge.charge(1000L)
-        println(charge.toLong())
+        val charge: BigDecimal = discharge.charge(1000L)
+        assertThat(charge).isEqualByComparingTo(BigDecimal.valueOf(900))
+    }
+
+    @Test
+    fun t3() {
+        val standard = Standard
+        val charge: BigDecimal = standard.charge(1000L)
+        assertThat(charge).isEqualByComparingTo(BigDecimal.valueOf(1000))
     }
 }
